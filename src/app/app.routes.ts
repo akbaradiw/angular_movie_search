0884dcfaa031394e943b0534d2df7802c839a4fb
akbaradiw <akbaradiw000@gmail.com>
2024-07-26 21:24:26 +0700

    fixing bug & make responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { MoviesComponent } from './pages/movies/movies.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
 
-    },
-    {
-        path: 'movies',
-        component: MoviesComponent
     }
+ 
 ];
--- src/app/component/card/card.component.ts
+++ src/app/component/card/card.component.ts
@@ -1,44 +1,52 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Ng2SearchPipe } from 'ng2-search-filter';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [ CommonModule, HttpClientModule ],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrls: ['./card.component.css'] 
 })
 export class CardComponent implements OnInit {
- httpsClient = inject(HttpClient);
- data: any[] = [];
- searchValue = '';
-ngOnInit(): void {
-  this.fetchData()
-}
-
-fetchData() {
-  this.httpsClient.get('https://www.omdbapi.com/?s=Title&apikey=263d22d8').subscribe(
-    (response: any) => {
-      console.log(response);
-      if (response.Search) {
-        this.data = response.Search;
-      }
-    },
-    (error: any) => {
-      console.error('Error fetching data:', error);
-    }
-  );
-}
-
-filter = (event: any) => {
-  console.log(event.target.value);
-  this.data = this.data.filter((item) => {
-    return item.Title.toLowerCase().includes(event.target.value.toLowerCase());
+  httpClient = inject(HttpClient);
+  fb = inject(FormBuilder); 
+  data: any[] = [];
+  filteredData: any[] = [];
+  searchForm = this.fb.nonNullable.group({
+    searchValue: ''
   });
-};
 
+  ngOnInit(): void {
+    this.fetchData();
+    this.searchForm.valueChanges.subscribe(value => {
+      this.filter(value.searchValue);
+    });
+  }
 
+  fetchData() {
+    this.httpClient.get('https://www.omdbapi.com/?s=Title&apikey=263d22d8').subscribe(
+      (response: any) => {
+        if (response.Search) {
+          this.data = response.Search;
+          this.filteredData = this.data;  
+        }
+      },
+      (error: any) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
 
-
+  filter(searchValue = '') {
+    if (!searchValue) {
+      this.filteredData = this.data;
+      return;
+    }
+    this.filteredData = this.data.filter((item) => {
+      return item.Title.toLowerCase().includes(searchValue.toLowerCase());
+    });
+  }
 }
